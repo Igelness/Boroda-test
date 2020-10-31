ac477b83ad597195d2081cb5711e77d4ef136796
Anton Vorobyov <igelness@gmail.com>
2020-10-31 16:48:00 +0300

    add about popup

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,27 @@ $('.features__slider').slick({
   dots: true,
   autoplay: false
 });
+
+const aboutDiv = document.querySelector(".header__about");
+const popup = document.querySelector(".about__popup-wrapper");
+const popupDiv = document.querySelector(".about__popup");
+const closePopup = document.querySelector(".about__popup-close");
+
+aboutDiv.addEventListener('click', () => {
+  popup.style.visibility = "visible";
+  popup.style.opacity = "0.9";
+  popupDiv.style.visibility = "visible";
+  popupDiv.style.opacity = "1";
+  popupDiv.style.top = "50%";
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target === popup || e.target === closePopup) {
+    popup.style.opacity = 0;
+    popup.style.visibility = "hidden";
+    popupDiv.style.opacity = 0;
+    popupDiv.style.visibility = "hidden";
+    popupDiv.style.top = "55%";
+  }
+});
+
