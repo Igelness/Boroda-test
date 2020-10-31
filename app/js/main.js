899ddaa176c7e462a205b730dc423772c18bc7de
Anton Vorobyov <igelness@gmail.com>
2020-10-31 19:17:33 +0300

    add slider popups

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+/* Slick Slider Settings */
+
 $('.features__slider').slick({
   infinite: true,
   slidesToShow: 2,
@@ -5,6 +7,8 @@ $('.features__slider').slick({
   dots: true,
   autoplay: false
 });
+
+/* About Popup */
 
 const aboutDiv = document.querySelector(".header__about");
 const popup = document.querySelector(".about__popup-wrapper");
@@ -19,6 +23,7 @@ aboutDiv.addEventListener('click', () => {
   popupDiv.style.top = "50%";
 });
 
+
 document.addEventListener('click', (e) => {
   if (e.target === popup || e.target === closePopup) {
     popup.style.opacity = 0;
@@ -26,6 +31,33 @@ document.addEventListener('click', (e) => {
     popupDiv.style.opacity = 0;
     popupDiv.style.visibility = "hidden";
     popupDiv.style.top = "55%";
+  }
+
+  /* Slider Popups */
+
+  if(e.target.classList.contains("link-activate")) {
+    e.preventDefault();
+    let hiddenInfo = e.target.parentElement.parentElement;
+    let hiddenBlock = hiddenInfo.querySelector(".features__slider-item-hidden");
+    if (hiddenBlock.classList.contains("new")) {
+      let hiddenBlockNew = hiddenBlock.querySelector(".hidden-new");
+      hiddenBlockNew.style.opacity = "1";
+      hiddenBlockNew.style.visibility = "visible";
+
+      let hiddenBlockImage = hiddenBlock.querySelector(".hidden-img").getElementsByTagName("img");
+      hiddenBlockImage[0].src = "images/hidden-new.png";
+      hiddenBlockImage[0].style.top = "0";
+
+    }
+    hiddenBlock.style.opacity = "1";
+    hiddenBlock.style.visibility = "visible";
+  }
+  if(e.target.classList.contains("link-disable")) {
+    e.preventDefault();
+    let hiddenBlock = e.target.parentElement.parentElement;
+
+    hiddenBlock.parentElement.style.opacity = "0";
+    hiddenBlock.parentElement.style.visibility = "hidden";
   }
 });
 
